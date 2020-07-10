a2866cd8b0b0a1c835fffd3e8e9ce5d1376c06e2
Jonson Petard <41122242+greenhat616@users.noreply.github.com>
2020-07-10 22:46:10 +0800

    fix: ci issue

--- test/checkJSONValid.js
+++ test/checkJSONValid.js
@@ -9,58 +9,58 @@ const rootDir = path.join(__dirname, '../')
 console.log('Check whether JSON files in JSON dir are valid.')
 
 // Step 1: generate files map...
-function readDir (path) {
-    const files = fs.readdirSync(path.join(rootDir, './json'))
-    if (files.length <= 0) return
-    const map = []
-    for (const file of path) {
-        const filePath = path.join(path, './', file)
-        const stat = fs.statSync(filePath)
-        if (stat.isDirectory()) {
-            const subDirFiles = readDir(filePath)
-            if (subDirFiles.length > 0) {
-                map.concat(subDirFiles)
-            }
-        } else {
-            map.push(filePath)
-        }
+function readDir (dir) {
+  const files = fs.readdirSync(dir)
+  if (files.length <= 0) return
+  const map = []
+  for (const file of dir) {
+    const filePath = path.join(dir, './', file)
+    const stat = fs.statSync(filePath)
+    if (stat.isDirectory()) {
+      const subDirFiles = readDir(filePath)
+      if (subDirFiles.length > 0) {
+        map.concat(subDirFiles)
+      }
+    } else {
+      map.push(filePath)
     }
-    return map
+  }
+  return map
 }
 const JSONFiles = readDir(path.join(rootDir, './json'))
 if (JSONFiles.length <= 0) {
-    console.log('Not found json files, test skipped')
-    process.exit(0)
+  console.log('Not found json files, test skipped')
+  process.exit(0)
 }
 
 // Step 2: valid JSON file
 const errors = []
 try {
-    for (const file of JSONFiles) {
-        console.log('test: ' + file)
-        if (file.substr(-5, 5) !== '.json') {
-            console.log('The file is not a json file, skipped')
-            continue
-        }
-        // DO Test
-        try {
-            JSON.parse(fs.readFileSync(file, {
-                encoding: 'utf8'
-            }))    
-        } catch (e) {
-            errors.push(['test: ' + file + ' failed', e])   
-        }
+  for (const file of JSONFiles) {
+    console.log('test: ' + file)
+    if (file.substr(-5, 5) !== '.json') {
+      console.log('The file is not a json file, skipped')
+      continue
     }
+    // DO Test
+    try {
+      JSON.parse(fs.readFileSync(file, {
+        encoding: 'utf8'
+      }))
+    } catch (e) {
+      errors.push(['test: ' + file + ' failed', e])
+    }
+  }
 } catch (e) {
-    errors.push(['unknown failure', e])
+  errors.push(['unknown failure', e])
 }
 
 if (errors.length > 0) {
-    console.error('test failed, found ' + errors.length + ' errors, show the below:')
-    errors.forEach(e => {
-        console.error(e[0])
-        console.error(e[1])
-    })
+  console.error('test failed, found ' + errors.length + ' errors, show the below:')
+  errors.forEach(e => {
+    console.error(e[0])
+    console.error(e[1])
+  })
 } else {
-    console.log('test successfully.')
+  console.log('test successfully.')
 }
